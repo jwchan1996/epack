9969ab6615407f0c08da7414e5f4b146febf244f
jwchan <741755613@qq.com>
2021-01-27 15:05:25 +0800

    feat: 读取配置文件并实例化Compiler类执行apply方法

--- bin/epack.js
+++ bin/epack.js
@@ -1,3 +1,18 @@
 #! /usr/bin/env node
 
-console.log('epack is log')
+const path = require('path')
+const { config } = require('process')
+
+// 获取获取配置文件信息
+const configPath = path.join(process.cwd(), 'webpack.config.js')
+// let configPath = path.join(__dirname, '../../../', 'webpack.config.js')
+
+// 按照路径来读取配置内容
+const configContent = require(configPath)
+// console.log(configContent)
+
+// 将当前获取到的文件信息传递给具体的业务功能实现操作
+// 下面代码表示会从 package.json 的 main 包入口文件导入
+const Compiler = require('..')  // 导入的是一个 Compiler 类
+const c = new Compiler(configContent)
+c.apply()
